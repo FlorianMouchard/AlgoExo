13ee3c02597af9c5a65d24355a49d6551830522d
FlorianMouchard <florian.mouchard@sfr.fr>
2018-06-01 09:20:40 +0200

    Début

---
+++ AlgoExo/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExo
+{
+    public class Menu
+    {
+        public int Numero { get; set; }
+        public string Titre { get; set; }
+    }
+}
---
+++ AlgoExo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int GererMenu(List<Menu> menu)
+        {
+            foreach (Menu line in menu)
+            {
+                Console.WriteLine($"{line.Numero}-{line.Titre}");
+            }
+            int choix;
+            choix = int.Parse(Console.ReadLine());
+
+            foreach (Menu item in menu)
+            {
+                if (item.Numero == choix)
+                {
+                    return choix;
+                }
+            }
+            Console.WriteLine("Erreur lors de la saisie");
+            return -1;
+        }
+    }
+}
